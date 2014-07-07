257a2340ed9ae067ea4a816639c0a80cc3e8f9b6
Eric Trinh <et.trinity@gmail.com>
2014-07-06 23:21:25 -0500

    Scripts for setting up sublime and zsh

---
+++ setup_sublime.sh
@@ -0,0 +1,3 @@
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+rm -r User
+ln -s ~/Dropbox/Sublime/User
---
+++ setup_zsh_prompt.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/sindresorhus/pure.git
+ln -s "$PWD/pure/pure.zsh" ~/.zprezto/modules/prompt/functions/prompt_pure_setup
