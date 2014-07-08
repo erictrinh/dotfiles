ceff3c6cf1184724ccf38aa5f879a1c157fb9bbf
Eric Trinh <et.trinity@gmail.com>
2014-07-08 01:12:08 -0500

    Replace sublime icon, sublime resources

---
+++ sublime/replace_sublime_icon.sh
@@ -0,0 +1,12 @@
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SUBLIME_PATH="$HOME/Applications"
+SUBLIME_3_NAME="Sublime Text.app"
+
+sudo cp "$THIS_DIR/SublimeText.icns" "$SUBLIME_PATH/$SUBLIME_3_NAME/Contents/Resources/Sublime Text.icns"
+
+# trigger refresh of app icon
+SUBLIME_COPY="SUBLIME COPY"
+cd -P "$SUBLIME_PATH/$SUBLIME_3_NAME/.."
+sudo cp -r "$SUBLIME_3_NAME" "$SUBLIME_COPY"
+sudo rm -rf "$SUBLIME_3_NAME"
+sudo mv "$SUBLIME_COPY" "$SUBLIME_3_NAME"
