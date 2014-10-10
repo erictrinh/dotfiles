c7c99067f262b8d7f9ea3635b5fae599d7c8c461
Eric Trinh <et.trinity@gmail.com>
2014-10-10 02:06:06 -0500

    Add apps and fonts

--- home/.hydra/config.lua
+++ home/.hydra/config.lua
@@ -1,11 +1,13 @@
 ext.config = {
-  A = 'Atom',
   F = 'Finder',
   C = 'Google Chrome',
+  X = 'Xcode',
   G = 'GitHub',
   S = 'Sublime Text',
   Y = 'Spotify',
+  L = 'Calendar',
   M = 'Messages',
+  N = 'Mailbox (Beta)',
   H = 'HipChat',
   T = 'iTerm',
   P = 'Preview'
