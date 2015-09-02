687f988a79505818a6d87babf0a50efafb15e706
Eric Trinh <et.trinity@gmail.com>
2015-09-01 21:31:13 -0500

    More shortcuts for hydra

--- home/.hydra/config.lua
+++ home/.hydra/config.lua
@@ -1,9 +1,11 @@
 ext.config = {
   F = 'Finder',
   C = 'Google Chrome',
+  W = 'Whiskey',
+  E = 'Slack',
   X = 'Xcode',
   K = 'Sketch',
-  G = 'GitHub',
+  G = 'GitHub Desktop',
   S = 'Sublime Text',
   Y = 'Spotify',
   L = 'Calendar',
